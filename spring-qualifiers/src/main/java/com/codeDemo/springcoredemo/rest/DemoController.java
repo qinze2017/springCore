ae4e6cbee615063301865e0c9a3cd8d47075d5be
qinze2017 <qinze2002@126.com>
2023-12-05 16:53:00 -0500

    base knowledge Spring core

---
+++ spring-qualifiers/src/main/java/com/codeDemo/springcoredemo/rest/DemoController.java
@@ -0,0 +1,26 @@
+package com.codeDemo.springcoredemo.rest;
+
+import com.codeDemo.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Coach myCoach;
+
+    @Autowired
+    public void DemoController(@Qualifier("tennisCoach") Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+
+    
+
+}
